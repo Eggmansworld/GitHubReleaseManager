3e23fc474011148626c0601484f01a4ccabe029f
Eggman <gilmorejamesadam@gmail.com>
2026-04-14 20:23:35 -0300

    Delete batch_files directory

--- batch_files/!Eggman's GitHub Release Manager (auto-close).bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "C:\Utilities\Eggman's GitHub Release Manager"
-python "Eggmans_GitHub_Release_Manager.py"
--- batch_files/!Eggman's GitHub Release Manager (no console).bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "C:\Utilities\Eggman's GitHub Release Manager"
-pythonw "Eggmans_GitHub_Release_Manager.py"
